3ea69fa7abce418682e58a7b159bed63eee2fbfd
bobaram <otintap@gmail.com>
2023-06-07 01:22:39 +0200

    Implementation of GET/  Endpoint to get all collections names

---
+++ routes/collections.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const router = express.Router();
+const config = require("config");
+const { MongoClient } = require("mongodb");
+
+const fetchCollectionNames = async () => {
+  const uri = config.get("db");
+  const client = new MongoClient(uri);
+  try {
+    await client.connect();
+    const database = client.db();
+    const collections = await database.listCollections().toArray();
+    const collectionNames = collections.map((collection) => collection.name);
+    const filteredCollectionNames = collectionNames.filter(
+      (name) => name !== "sentences"
+    );
+
+    return filteredCollectionNames;
+  } catch (error) {
+    throw error;
+  } finally {
+    await client.close();
+  }
+};
+
+router.get("/", async (req, res) => {
+  const collectionNames = await fetchCollectionNames();
+  if (!collectionNames)
+    return res.status(500).send("Could not get collection names!");
+
+  return res.status(200).send(collectionNames);
+});
+
+module.exports = router;
+exports.fetchCollectionNames = fetchCollectionNames;
--- startup/routes.js
+++ startup/routes.js
@@ -2,11 +2,13 @@ const express = require("express");
 const error = require("../middleware/error");
 const wordsRouter = require("../routes/words");
 const sentenceRouter = require("../routes/sentences");
+const collectionNameRouter = require("../routes/collections");
 const config = require("config");
 
 module.exports = function (app) {
   app.use(express.json());
   app.use(config.get("wordsEndpoint"), wordsRouter);
   app.use(config.get("sentencesEndpoint"), sentenceRouter);
+  app.use(config.get("collectionNamesEndpoint"), collectionNameRouter);
   app.use(error);
 };
